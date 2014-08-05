7098857c93a507860a0b9deb9246029c34ab9a4c
Marina's Live Sever <marinahuber@g.harvard.edu>
2014-08-05 19:14:09 -0400

    many commit

--- vendor/adavkay/laravel-soundcloud/src/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-    'client_id' => '23e299b5e8ba4224cdd81b13119acfa2',
-    'client_secret' => '57c1f8ac7834af14e64d236dcc6576b8',
-    'redirect_uri' => 'https://soundcloud.com/selina-huber-1'
-);
